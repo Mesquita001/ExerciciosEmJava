9dbfc3059394449cd6e063339ca7d52795d5dc41
Andre Mesquita <mesquitas@hotmail.com>
2021-12-07 20:34:10 -0300

    contador de pernas

---
+++ ProgramaPernas/src/programapernas/ProgramaPernas.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programapernas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class ProgramaPernas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Quantas pernas? ");
+        int perna = tec.nextInt();
+        String tipo;
+        System.out.println("Isso é um(a) ");
+        switch (perna) {
+            case 1:
+               tipo = "Sasci";
+               break;
+            case 2:
+                tipo = "Bipede";
+                break;
+            case 3:
+                tipo = "Tripé";
+                break;
+            case 4:
+                tipo = "Quadrúpede";
+                break;
+            case 6:
+                tipo = "Aranha";
+                break;
+            default:
+                tipo = "ET";
+                break;
+        }
+        System.out.println(tipo);
+    }
+    
+}
